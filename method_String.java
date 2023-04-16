3b1374428312180dcb0b500f31e9bf55f5231b9b
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-04-16 13:12:14 +0530

    first commit

---
+++ method_String.java
@@ -0,0 +1,20 @@
+public class method_String {
+    public static void main(String[] args) {
+        String name = "Aman Raza";
+        
+        System.out.println(name.length());
+        System.out.println(name.toLowerCase());
+        // System.out.println(name.toUpperCase());
+        // String nontrim = "    Aman    Raza   ";
+        // System.out.println(nontrim.trim());
+
+        // System.out.println(name.substring(4));
+        // System.out.println(name.substring(4,8));
+        // System.out.println(name.replace("Raza", "Khan"));
+        // System.out.println(name.startsWith("Ama"));
+        // System.out.println(name.endsWith("a"));
+        // System.out.println(name.charAt(5));
+        // System.out.println(name.indexOf("n"));
+    }
+    
+}
