c15c7b631fe9b1786728fb4538cdc9d61bb7f10c
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-04-20 13:12:14 +0530

    first commit

---
+++ prac_set1.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class prac_set1 {
+    public static void main(String[] args) {
+        int a = 24;
+        int b = 25;
+        int c = 28;
+        int sum = a+b+c;
+        System.out.println(sum);
+    }
+}
+
+// question 2
+/*
+public class prac_set1 {
+    public static void main(String[] args) {
+        float a = 45.0f;
+        float b = 95.0f;
+        float c = 48.0f;
+        float sum = (a+b+c)/30;
+        System.out.println(sum);
+    }
+}
+*/
+
+/* Question 3
+public class prac_set1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello "+name+", here a good day");
+
+    }
+}
+*/
+
+
+ /*
+ Question 4
+public class prac_set1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float kilo_meter = sc.nextFloat();
+        float miles = kilo_meter * 0.6213714f;
+        System.out.println(miles);
+    }
+}
+*/
+
+
+// Question 5
+public class prac_set1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
