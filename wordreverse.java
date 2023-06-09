d5c21a1cc0f567b8f55eb515e50c06e46c3f1d2c
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-06-09 13:12:14 +0530

    commit

---
+++ wordreverse.java
@@ -0,0 +1,11 @@
+public class wordreverse {
+    public static void main(String[] args) {
+    //     String str = "This is a statement";
+    //     String k = "";
+    //     for(int i =str.length()-1; i>=0; i--){
+    //         k = k+ str.charAt(i);
+            
+    //     }
+    //     System.out.println(k);
+    }
+}
