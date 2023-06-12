39b0096c036eb7a9975dba43370aeec82c55328b
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-06-12 09:54:38 +0530

    Add files via upload

---
+++ patternusingsout.java
@@ -0,0 +1,10 @@
+package pattern;
+
+public class patternusingsout {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
