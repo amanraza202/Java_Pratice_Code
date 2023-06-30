3d7654a1eefcfc8866ea1e8f4d9add3162515589
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-06-30 13:12:14 +0530

    First commit

---
+++ arrayList.java
@@ -0,0 +1,16 @@
+package arrayList;
+import java.util.*;
+
+public class arrayList{
+    public static void main(String[] args) {
+       
+        ArrayList<Integer> l1 = new ArrayList<>();
+        l1.add(1);
+        l1.add(5);
+        l1.add(8);
+        l1.add(810);
+        l1.add(89);
+        // System.out.println(l1.isEmpty());
+        System.out.println(l1.lastIndexOf(89));
+    }
+}
