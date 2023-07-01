9310994462a231e80f5e512df9769ae3351a12eb
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-07-01 13:12:14 +0530

    all DataType

---
+++ datatype.java
@@ -0,0 +1,21 @@
+package DataType;
+
+public class datatype {
+    public static void main(String[] args) {
+        // DataType
+        int a =10;
+        float b= 12.5f;
+        Byte c = 10;
+        long d = 1000000000000l;
+        double e = 14.55;
+        boolean f = true;
+        boolean g = false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
