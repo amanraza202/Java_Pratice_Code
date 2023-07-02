de210d609f2e821d7a8ccb3f34170d68504affe1
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-07-02 13:12:14 +0530

    first commit

---
+++ classAndPointer.java
@@ -0,0 +1,24 @@
+class Cookie{
+    private String name;
+
+    public Cookie(String Color){
+        name = Color;
+
+    }
+    public String getColor(){
+        return name;
+    }
+    public void setColor(String color){
+        name = color;
+    }
+}
+public class classAndPointer {
+    public static void main(String[] args) {
+        Cookie obj = new Cookie("Green");
+        // String ns = obj.getColor();
+        // System.out.println(ns);
+        // After setting setcolor 
+        // obj.setColor("Blue");
+        // System.out.println(obj.getColor());
+    }
+}
