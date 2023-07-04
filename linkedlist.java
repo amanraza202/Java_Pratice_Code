6fc2ec76bba5b211a799f0f47ab781f6e2329d1e
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-07-04 13:12:14 +0530

    get Node

--- linkedlist.java
+++ linkedlist.java
@@ -95,32 +95,32 @@ class ll {
         length++;
     }
 
-    // public Node removeFirst() {
-    //     if (length == 0) {
-    //         return null;
-    //     }
-    //     Node temp = head;
-    //     head = head.next;
-    //     temp.next = null;
-    //     length--;
+    public Node removeFirst() {
+        if (length == 0) {
+            return null;
+        }
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
 
-    //     if (length == 0) {
-    //         tail = null;
-    //         head = null;
-    //     }
-    //     return temp;
-    // }
+        if (length == 0) {
+            tail = null;
+            head = null;
+        }
+        return temp;
+    }
 
-    // public Node get(int index){
-    //     if( index<0 || index>=length){
-    //         return null;
-    //     }
-    //     Node temp = head;
-    //     for(int i =0 ;i<index; i++){
-    //         temp = temp.next;
-    //     }
-    //     return temp;
-    // }
+    public Node get(int index){
+        if( index<0 || index>=length){
+            return null;
+        }
+        Node temp = head;
+        for(int i =0 ;i<index; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
 
     // public boolean set(int index, int value){
     //     Node temp = get(index);
