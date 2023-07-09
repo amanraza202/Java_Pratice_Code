751672695536fc49ccedeba7966f7b6659e7542a
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-07-09 13:12:14 +0530

    first commit

---
+++ allBasic.java
@@ -0,0 +1,17 @@
+package GeeksForGeeks;
+
+public class allBasic {
+    public static void main(String[] args) {
+        // sum of two binary number
+        String num = "110";
+        String num1 = "011";
+
+        int n1 = Integer.parseInt(num,2);
+        int n2 = Integer.parseInt(num1,2);
+        int sum = n1+n2;
+
+        System.out.println(Integer.toBinaryString(sum));
+
+        // sum of imaginary number;
+    }
+}
