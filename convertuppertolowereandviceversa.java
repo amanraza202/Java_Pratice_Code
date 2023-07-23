1dc2523a4f8989d4c5424792e75e8c0918f767d9
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-07-23 13:12:14 +0530

    first commit

---
+++ convertuppertolowereandviceversa.java
@@ -0,0 +1,30 @@
+package StringBuilder;
+
+public class convertuppertolowereandviceversa {
+    public static void main(String[] args) {
+        String str1 ="Physics";
+        StringBuilder str = new StringBuilder(str1);
+        for(int i =0; i<= str.length()-1; i++){
+            char ch = str.charAt(i);
+            if(ch ==' '){
+                continue;
+            }
+            int asci = (int)ch;
+            Boolean flag = true;
+            if(asci>=97){
+                flag = false;
+            }
+            if(flag == true){
+                asci += 32;
+                char dh = (char)asci;
+                str.setCharAt(i, dh);
+            }
+            else{
+                asci -=32;
+                char dh = (char)asci;
+                str.setCharAt(i, dh);
+            }
+        }
+        System.out.println(str);
+    }
+}
