c475860ade6985133015656ea9cd0809036b15c7
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-09 13:12:14 +0530

    first commit

---
+++ concatAndReverseString.java
@@ -0,0 +1,15 @@
+// package String;
+
+public class concatAndReverseString {
+    public static void main(String[] args) {
+        String str1 = "Geeks";
+        String str2 = "forGeeks";
+        String res = str1+str2;
+        // StringBuilder sb = new StringBuilder(res);
+        // sb.reverse();
+        // System.out.println(sb);
+
+        int a = str1.codePointBefore(2);
+        System.out.println(a);
+    }
+}
