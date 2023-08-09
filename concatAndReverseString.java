350046d5e27c678839b9aede3ce6e3287cb47d7c
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-09 13:12:14 +0530

    first commit

--- concatAndReverseString.java
+++ concatAndReverseString.java
@@ -5,9 +5,9 @@ public class concatAndReverseString {
         String str1 = "Geeks";
         String str2 = "forGeeks";
         String res = str1+str2;
-        // StringBuilder sb = new StringBuilder(res);
-        // sb.reverse();
-        // System.out.println(sb);
+        StringBuilder sb = new StringBuilder(res);
+        sb.reverse();
+        System.out.println(sb);
 
         int a = str1.codePointBefore(2);
         System.out.println(a);
