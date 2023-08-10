99cb79551ba54fec5348bc4dc56703a62da5cf8f
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-10 13:12:14 +0530

    first commit

---
+++ getCharacter.java
@@ -0,0 +1,19 @@
+package String;
+import java.util.Scanner;
+class getCharacter{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int index = sc.nextInt();
+        String str = "GeeksForGeeks";
+        
+        for(int i =0; i<str.length(); i++){
+           char ch = str.charAt(index);
+           System.out.println(ch);
+           break;
+
+        }
+        int idx = 5;
+        StringBuilder sb = new StringBuilder("GeeksForGeeks");
+        System.out.println(sb.charAt(idx));
+    }
+}
