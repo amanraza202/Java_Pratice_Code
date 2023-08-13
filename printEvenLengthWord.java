c3215814fa43508d9316750c68e79b8754509601
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-13 13:12:14 +0530

    first commit

---
+++ printEvenLengthWord.java
@@ -0,0 +1,13 @@
+package String;
+
+public class printEvenLengthWord {
+    public static void main(String[] args) {
+        String str[] = "This is a java".split(" ");
+        for (String word : str) {
+            if(word.length()%2 ==0){
+                System.out.println(word);
+            }
+        }
+
+    }
+}
