e7315718fa385e2de54b9ead4d87c5ee6ca1714c
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-16 13:12:14 +0530

    first commit

---
+++ sortString.java
@@ -0,0 +1,13 @@
+// package String;
+import java.util.*;
+class sortString{
+    public static void main(String[] args) {
+        // StringBuilder sb = new StringBuilder("geeksforgeeks");
+        String str = "geeksforgeeks";
+        for(int i =0; i<str.length();i++){
+            char ch = str.charAt(i);
+            int dh = (int) ch;
+            
+        }
+    }
+}
