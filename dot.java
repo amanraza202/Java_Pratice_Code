8390e42e06aab2ad9d856c781bfeda457edd4fd3
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-24 13:12:14 +0530

    first commit

--- dot.java
+++ dot.java
@@ -2,9 +2,9 @@ import java.util.regex.*;
 public class dot {
     public static void main(String[] args) {
         String str = "12345789551 ABC";
-        // Pattern pattern = Pattern.compile("Welcome.");
+        Pattern pattern = Pattern.compile("Welcome.");
         // Pattern pattern = Pattern.compile("\\d"); //accept all digit only
-        Pattern pattern = Pattern.compile("\\D"); //except digit all accept
+        // Pattern pattern = Pattern.compile("\\D"); //except digit all accept
         Matcher match = pattern.matcher(str);
         while(match.find()){
             System.out.print(str.substring(match.start(), match.end()));
