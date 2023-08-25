9d70c075490d66218b40bbd9f46d81a23812c9ee
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-25 13:12:14 +0530

    first commit

--- example.java
+++ example.java
@@ -4,8 +4,8 @@ public class example {
     public static void main(String[] args) {
         // String str ="Hello1";
         String str ="Hello1";
-        // Pattern pattern = Pattern.compile("[a-zA-Z]+[0-9]");
-        Pattern pattern = Pattern.compile("\\w+\\d");
+        Pattern pattern = Pattern.compile("[a-zA-Z]+[0-9]");
+        // Pattern pattern = Pattern.compile("\\w+\\d");
         Matcher matcher = pattern.matcher(str);
 
         if(matcher.matches()){
