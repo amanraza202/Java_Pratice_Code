f183fba6bffb170358644d6a02f43bee339e6fdf
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-27 13:12:14 +0530

    first commit

--- regex.java
+++ regex.java
@@ -2,10 +2,10 @@ import java.util.regex.*;
 
 class regex {
     public static void main(String[] args) {
-        // Pattern pattern = Pattern.compile("[abc]");// only abc Matches
+        Pattern pattern = Pattern.compile("[abc]");// only abc Matches
         // Pattern pattern = Pattern.compile("[a-z]");// all a to z Matches only small alphabet
         // Pattern pattern = Pattern.compile("[0-9]");// all 0 to 9 Matches only small alphabet
-        Pattern pattern = Pattern.compile("[^abc]");// Except abc Matches
+        // Pattern pattern = Pattern.compile("[^abc]");// Except abc Matches
         Matcher match = pattern.matcher("a");
 
         if (match.matches()) {
