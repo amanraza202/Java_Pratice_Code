d0bf809dfcaf1fddfa2fa368c63621bf7f0a0dfc
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-30 13:12:14 +0530

    first commit

---
+++ string.java
@@ -0,0 +1,27 @@
+// package String;
+class string{
+    public static void main(String[] args) {
+        String str = "hello";
+        String str1 = "abc";
+        System.out.println(str.length());
+        // System.out.println(str.charAt(3));
+        // System.out.println(str.indexOf('l'));
+        // System.out.println(str.compareTo(str1));
+        // System.out.println(str.contains("hel"));
+        // System.out.println(str.startsWith("h"));
+        // System.out.println(str.endsWith("lo"));
+        // System.out.println(str.toLowerCase());
+        // System.out.println(str.toUpperCase());
+        // String s1 ="abc";
+        // String s2 = "def";
+        // String s =s1.concat(s2);
+        // System.out.println(s);
+
+        // String str = "hello";
+        // String btr = new String("hello");
+        // System.out.println(str.equals(btr));
+        // System.out.println(str==btr);
+
+
+    }
+}
