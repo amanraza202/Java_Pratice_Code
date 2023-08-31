ca767aad89a37dbb9dd6ecc5e88900540f0e6c90
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-31 13:12:14 +0530

    first commit

--- stringintchar.java
+++ stringintchar.java
@@ -2,7 +2,7 @@
 
 public class stringintchar {
     public static void main(String[] args) {
-        String str = "abcdefghijklm";
+        String str = "abcdefghim";
         System.out.println(str.substring(1,5));
         System.out.println(str.substring(5));
     }
