bdbf3d08d8a2da659462ae2641bdb5ccc19947fb
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-08-31 13:12:14 +0530

    first commit

---
+++ stringintchar.java
@@ -0,0 +1,9 @@
+// package String;
+
+public class stringintchar {
+    public static void main(String[] args) {
+        String str = "abcdefghijklm";
+        System.out.println(str.substring(1,5));
+        System.out.println(str.substring(5));
+    }
+}
